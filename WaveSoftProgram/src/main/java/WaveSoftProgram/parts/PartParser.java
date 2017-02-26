82ecf11e597cc9987875429bd72f10096e96512e
Piotr Górny <piotr_gy@wp.pl>
2017-02-26 13:58:50 +0100

    Trying process json as tree model

--- WaveSoftProgram/src/main/java/WaveSoftProgram/parts/PartParser.java
+++ WaveSoftProgram/src/main/java/WaveSoftProgram/parts/PartParser.java
@@ -1,22 +1,56 @@
 package WaveSoftProgram.parts;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.codehaus.jackson.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/*import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectMapper;*/
 /**
  * Created by piotr_gy on 22.02.17.
  */
 public class PartParser {
+private List<PlaceInCar>placeInCar = new ArrayList<PlaceInCar>();
+
     public PartParser() {
+
         try {
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode rootArray = mapper.readTree(new File("./WaveSoftProgram/src/main/resources/parts.json"));
+            PartCategory partCategory  = new PartCategory();
+
+            for(JsonNode root : rootArray){
+
+                placeInCar.getNameOfPlace().add()= root.path("name_of_place").asText();
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+        //full data binding - doesn't work
+        /*
             ObjectMapper mapper = new ObjectMapper();
             PlaceInCar placeInCar = mapper.readValue(new File("./WaveSoftProgram/src/main/resources/parts.json"), PlaceInCar.class);
 
 
-            System.out.println(placeInCar);
+            System.out.println(placeInCar);*/
 
         }
         catch (JsonParseException e) { e.printStackTrace(); }
