b518eb54e28634f8d12924abaf86c957982151ed
piotrgy <piotr_gy@wp.pl>
2017-03-01 21:35:14 +0100

    The main class - menu - initial commit

---
+++ WaveSoftProgram/src/main/java/WaveSoftProgram/FindingPartOnAllegro.java
@@ -0,0 +1,19 @@
+package WaveSoftProgram;
+
+import WaveSoftProgram.parts.PartParser;
+
+/**
+ * Created by piotr_gy on 01.03.17.
+ */
+public class FindingPartOnAllegro {
+
+    private PartParser partParser;
+
+    public void findPartMenu(){
+        partParser = new PartParser();
+        int index = 1;
+
+
+
+    }
+}
